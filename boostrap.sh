4734af5e581cd826cbce9d1c30f4e450f92a646c
dbgeek <bjorn.ahl@gmail.com>
2017-07-29 21:38:11 +0200

    Refact a variable name

--- boostrap.sh
+++ boostrap.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env BASH
 cd "$(dirname "${BASH_SOURCE}")"
 
-
-for rec in $(ls -1A | grep -v .git); do
-  if [[ -f ~/"$rec" ]]; then
-    if [[ -L ~/"$rec" ]]; then
-      echo item: "$rec"
+for dotfile in $(ls -1A | grep -v .git); do
+  if [[ -f ~/"$dotfile" ]]; then
+    if [[ -L ~/"$dotfile" ]]; then
+      echo item: "$dotfile"
     else
-      echo "file exists: ${rec}"
-      mv ~/$rec ~/$rec_$(date +%Y%m%d%H%M%S)
-      ln -s $(pwd)/${rec} ~/${rec}
+      echo "file exists: ${dotfile}"
+      mv ~/${dotfile} ~/${dotfile}_$(date +%Y%m%d%H%M%S)
+      ln -s $(pwd)/${dotfile} ~/${dotfile}
     fi
   else
     echo "file not exists: ${rec}"
     ln -s $(pwd)/${rec} ~/${rec}
   fi
 done
+
