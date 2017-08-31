a7f21430a73e17ee13add9d348fc012ed1a8a37f
Bjorn Ahl <bjorn.ahl@klarna.com>
2017-08-31 14:49:55 +0200

    gnupg into dotfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 shopt -s nullglob dotglob
-IGNORE=(".git" ".gitignore" "bootstrap.sh")
+IGNORE=(".git" ".gitignore" "bootstrap.sh" "gnupg")
+PLATFORM=$(uname)
 
 cd "$(dirname "${BASH_SOURCE[@]}")" || exit
 
@@ -22,12 +23,31 @@ main () {
       if [[ ! -L ~/"$dotfile" ]]; then
         mv ~"/${dotfile}" ~"/${dotfile}_$(date +%Y%m%d%H%M%S)"
         ln -s "$(pwd)/${dotfile}" ~"/${dotfile}"
+        echo "backup files"
       fi
     elif ! array_contains "$dotfile" "${IGNORE[@]}"; then
       echo "file not exists: ${dotfile}"
       ln -s "$(pwd)/${dotfile}" ~"/${dotfile}"
     fi
   done
+
+
+  if [[ "$PLATFORM"  == "Darwin" ]]; then
+    if [[ ! -d ~/".gnupg" ]]; then
+      mkdir ~/".gnupg"
+    fi
+    for file in gnupg/osx/*; do
+      FILENAME=$(basename "$file")
+      if [[ -f ~/".gnupg/$FILENAME" ]]; then
+        if [[ ! -L ~/".gnupg/$FILENAME" ]]; then
+          mv "$HOME/.gnupg/${FILENAME}" "$HOME/.gnupg/${FILENAME}_$(date +%Y%m%d%H%M%S)"
+          ln -s "$(pwd)/${file}" "$HOME//.gnupg/${FILENAME}"
+        fi
+      else
+          ln -s "$(pwd)/${file}" "$HOME/.gnupg/${FILENAME}"
+      fi
+    done
+  fi
 }
 
 main
